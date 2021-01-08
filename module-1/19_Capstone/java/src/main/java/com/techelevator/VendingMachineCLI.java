e2b571ab042f401b1e867dd5adccb84a8c1f6479
Connor Corsini <burdoc725@gmail.com>
2021-01-08 12:05:26 -0500

    Capstone

--- module-1/19_Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ module-1/19_Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -7,6 +7,11 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION };
 
 	private Menu menu;
 
@@ -14,14 +19,30 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
+	public void runPurchaseMenu() {
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+			
+			if (choice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
+				System.out.println("Feed Money"); // TODO prompt for an amount
+			} else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+				System.out.println("Select Product");
+			} else if (choice.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION)) {
+				System.out.println("Finish Transaction"); // TODO make change
+				return;
+			}
+		}
+	}
+	
 	public void run() {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
+			
+				System.out.println("items");// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				runPurchaseMenu();// do purchase
 			}
 		}
 	}
